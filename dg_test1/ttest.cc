ecd961b60bf841260facffe0f5d26c3600ebfb80
shrisri <shriya26s@gmail.com>
2019-07-03 02:13:55 -0700

    Updating files

--- dg_test1/ttest.cc
+++ dg_test1/ttest.cc
@@ -1,3 +1,10 @@
+/*
+ * Objective:Generates 200 random strings of random sizes and writes them to random offsets of file newfile.txt * as well as writes the metadata in the form of character,size,offset into file newfile2.txt.
+ *
+ * Author: Shriya S
+ *
+ */
+
 #include "cstdio"
 #include "iostream"
 #include "cstdlib"
@@ -18,10 +25,13 @@ int main(){
         std::fstream fp2;
         //Creating a file called newfile.txt to write strings at different offsets, in writing mode.
         fp.open("newfile.txt",std::ios::out);
+
         //Creating a file called newfile2.txt to write metadata in the form of char, size, offset.
         fp2.open("newfile2.txt",std::ios::out);
+
         //srand() is a function that makes sure that offset values are different each time generated.
         srand(time(0));
+
         int i=0;
         //Loop generates random num, offset and rsize 200 times.
         while (i<=NO_DATA){
@@ -31,22 +41,29 @@ int main(){
             if (char(num)=='\\' || char(num)==' ' || char(num)=='?'){
                 continue;
             }
+
             char rstr[25]="";
             for (int j=0;j<rsize;j++){
                 rstr[j]=char(num);
             }
+
             //seekp() seeks the random offset in newfile.txt.
             fp.seekp(offset,std::ios::beg);
+
             int act=fp.tellp();
+
             //Writing string rstr to newfile.txt.
             fp<<rstr;
+
             //Writing char, size, offset to newfile2.txt.
             fp2<<rstr[0]<<" "<<rsize<<" "<<act<<'\n';
+
             i++;
         
         }
-    cout<<"Wrote 200 strings into newfile.txt at different offsets and metadata in newfile2.txt"<<'\n';
-        
+        cout<<"Wrote 200 strings into newfile.txt at different offsets and metadata in newfile2.txt"<<'\n';
+
+        // Close newfile.txt
         fp.close();
 }
 
